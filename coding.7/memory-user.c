5c76f7d5e934bcbe6da32c9fd40c8f2d59f1b984
qasema <qasema@berea.edu>
2023-09-28 19:21:16 -0400

    update coding 7

---
+++ coding.7/memory-user.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+int main(int argc, char *argv[])
+{
+    if (argc != 2) {
+        fprintf(stderr, "missing memory arg \n");
+        exit(1);
+    }
+    long long megabytes = atoll(argv[1]);
+    long long arr_size = megabytes * 1024 * 1024;  // Calculate array size in bytes
+    char *arr = (char *)malloc(arr_size);
+    if (arr == NULL) {
+        fprintf(stderr, "Memory allocation failed.\n");
+        return 1;
+    }
+    printf("Running with %lld bytes\n", arr_size);
+
+    // Touch each byte in the array
+    while (1) {
+        for (long long i = 0; i < arr_size; i++)
+            arr[i] += 1;
+    }
+
+    free(arr);
+    return 0;
+}
